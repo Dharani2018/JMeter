e8e570c6d72928665ba1837b87c2940031024fa2
Dharani2018 <dharaniparimella13@gmail.com>
2025-02-13 11:54:24 -0800

    Experimenting.

--- CRRS/src/main/java/com/dp/test/Hello1.java
+++ CRRS/src/main/java/com/dp/test/Hello1.java
@@ -1,10 +1,35 @@
 package com.dp.test;
 
-class Hello1 {
+public class Hello1{
+	final int a=23;
+	 int b=23;
+	 float c=5.2f;
+	 
+		/*
+		 * byte d=(byte)b; String e="kiran"; boolean k= b!=c & c>a;
+		 */
 	
-	public static void main(String[] args) {
-		Hello h = new Hello();
-		h.greet();
-	}
+	 public void diff() {
+	  
+	   if (a>b) {System.out.println(a-b);}
+		
+	  else if(b>a) { System.out.println(b-a); } 
+		 
+	  else {System.out.println("a and b are equal");}
+    }
 
+}
+   class Hello extends Hello1{
+    
+    	
+	 public static void main(String[] args) 
+    { 
+
+        System.out.println("Welcome to java world"); 
+
+        Hello1 h = new Hello1();
+        h.diff();
+
+  
+    } 
 }
